5d32c52276569e21607c6e5d3812237434e652d4
Dmytro-Leonov <leonovdmitriy02@gmail.com>
2023-05-11 20:30:46 +0300

    fix user admin

--- django_app/expenses_control/users/admin.py
+++ django_app/expenses_control/users/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from expenses_control.users.models import User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    pass
+class UserAdmin(BaseUserAdmin):
+    list_display = ("username", "email", "is_active", "is_staff", "is_superuser")
+
+    search_fields = ("username", "email")
+
+    list_filter = ("is_active", "is_staff", "is_superuser")
+
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        ("Permissions", {"fields": ("groups", "user_permissions", "is_active", "is_staff", "is_superuser")}),
+        ("Timestamps", {"fields": ("last_login",)}),
+    )
+    add_fieldsets = (
+        (None, {"fields": ("username", "email", "password1", "password2")}),
+        ("Permissions", {"fields": ("groups", "user_permissions", "is_active", "is_staff", "is_superuser")}),
+    )
+
+    readonly_fields = ("last_login",)
